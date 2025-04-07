e849919211ae4a250dee14d3165662e027acc280
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2025-04-06 23:59:22 -0500

    add camel case matcher spec

---
+++ ruby/spec/camel_case_matcher_spec.rb
@@ -0,0 +1,33 @@
+require_relative '../lib/camel_case_matcher'
+
+RSpec.describe '#camel_case_match?' do
+  it 'returns true for valid camelCase match' do
+    words = ["best", "time", "ever"]
+    camel = "bestTimeEver"
+    expect(camel_case_match?(camel, words)).to eq(true)
+  end
+
+  it 'returns false if camel case word has extra characters' do
+    words = ["best", "time"]
+    camel = "bestTimeForever"
+    expect(camel_case_match?(camel, words)).to eq(false)
+  end
+
+  it 'returns false if word is not in the list' do
+    words = ["cool", "fun"]
+    camel = "coolTimeFun"
+    expect(camel_case_match?(camel, words)).to eq(false)
+  end
+
+  it 'returns true for single word' do
+    words = ["hello"]
+    camel = "hello"
+    expect(camel_case_match?(camel, words)).to eq(true)
+  end
+
+  it 'returns false if camel case uses duplicate word not listed twice' do
+    words = ["fun"]
+    camel = "funFun"
+    expect(camel_case_match?(camel, words)).to eq(false)
+  end
+end
